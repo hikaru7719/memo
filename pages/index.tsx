8db88a978259dda72f34a370fe3ba05914e207bb
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2019-09-24 00:00:10 +0900

    add text field

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
 import React, { useState } from "react";
-import { TextField } from "office-ui-fabric-react";
+import {
+  TextField,
+  Stack,
+  IStackProps,
+  PrimaryButton
+} from "office-ui-fabric-react";
+
 const Example: React.FC = () => {
+  const columnProps: Partial<IStackProps> = {
+    tokens: { childrenGap: 25 },
+    styles: { root: { width: 800 } }
+  };
+  const columnProps2: Partial<IStackProps> = {
+    styles: { root: { width: 100 } }
+  };
   const [count, setCount] = useState(0);
-  return <TextField label="memo"></TextField>;
+  return (
+    <Stack horizontal horizontalAlign="center" tokens={{ childrenGap: 50 }}>
+      <Stack {...columnProps}>
+        <TextField label="title" />
+        <TextField label="memo" multiline rows={3} />
+        <Stack.Item align="center" styles={columnProps2}>
+          <PrimaryButton text="Primary" />
+        </Stack.Item>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Example;
