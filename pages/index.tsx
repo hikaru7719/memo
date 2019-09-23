e62dcd60abde765ea0f06d9f2b9b5d981fdd8946
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2019-09-23 11:58:19 +0900

    success to build sample page

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Example: React.FC = () => {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
+
+export default Example;
