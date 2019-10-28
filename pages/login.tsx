1d63e1972a7bd37ca8235297ff273e8ff8a04ed3
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2019-10-28 22:54:09 +0900

    login

---
+++ pages/login.tsx
@@ -0,0 +1,40 @@
+import { Card, Icon } from "antd";
+import { Row, Col } from "antd";
+import { Typography } from "antd";
+import { Button } from "antd";
+export default () => {
+  const Title = Typography.Title;
+  return (
+    <div style={{ marginTop: 200 }}>
+      <Row type="flex" justify="center">
+        <Col span={8}>
+          <Card style={{ width: 500 }}>
+            <div style={{ textAlign: "center" }}>
+              <Title level={2}>アカウントを利用してログイン</Title>
+              <Button type="primary" block>
+                <Icon type="twitter" />
+                Login with Twitter
+              </Button>
+              <Button type="danger" block style={{ marginTop: 15 }}>
+                <Icon type="google" />
+                Login with Google
+              </Button>
+              <Button
+                type="ghost"
+                block
+                style={{
+                  marginTop: 15,
+                  backgroundColor: "#000",
+                  color: "#fff"
+                }}
+              >
+                <Icon type="github" />
+                Login with GitHub
+              </Button>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
