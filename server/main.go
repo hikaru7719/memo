75beb59360547e50f4d0f4c2e6c14eebeb9b74c1
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2019-11-01 23:49:14 +0900

    chnage package configuration

---
+++ server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/hikaru7719/memo/server/proto"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.HelloServiceServer(grpcServer, nil)
+	grpcServer.Serve(lis)
+}
